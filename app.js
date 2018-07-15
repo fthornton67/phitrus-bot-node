f7634ae70cc17763738e13949006681e5d311546
Fredrick <fthornton67@gmail.com>
2018-07-14 20:18:13 -0700

    add skill to bot

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+const Alexa = require('ask-sdk-core');
+const skillBuilder = Alexa.SkillBuilders.custom();
+
 
 const { BotStateSet, BotFrameworkAdapter, MemoryStorage, ConversationState, UserState } = require('botbuilder');
 const restify = require('restify');
@@ -25,8 +28,46 @@ adapter.use(new BotStateSet(convoState, userState));
 
 const echoBot = createEchoBot(convoState);
 
+server.get('/',(req,res)=>{
+  res.send("bot");
+});
 // Listen for incoming requests 
 server.post('/api/messages', (req, res) => {
     // Route received request to adapter for processing
     adapter.processActivity(req, res, echoBot);
 });
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Welcome to the phitrus skill, say hello!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  },
+};
+
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+exports.handler = skillBuilder
+  .addRequestHandlers(LaunchRequestHandler)
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
