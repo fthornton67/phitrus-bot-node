383f02d9c74d9d57e4483f9c10208b04436ba778
Fredrick Thornton <fthornton67@gmail.com>
2018-07-15 16:44:17 -0700

    update ask

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // This loads the environment variables from the .env file
 require('dotenv-extended').load();
+const Alexa = require('ask-sdk');
+
+
 
 var builder = require('botbuilder');
 var restify = require('restify');
