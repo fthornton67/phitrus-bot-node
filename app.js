c2055ebb844e18eadee88907b45fa96656ce345d
Fredrick Thornton <fthornton67@gmail.com>
2018-07-15 18:38:16 -0700

    update error handler

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ const LaunchRequestHandler = {
     },
   };
 
+  const ErrorHandler = {
+    canHandle() {
+      return true;
+    },
+    handle(handlerInput, error) {
+      console.log(`Error handled: ${error.message}`);
+  
+      return handlerInput.responseBuilder
+        .speak('Sorry, I can\'t understand the command. Please say again.')
+        .reprompt('Sorry, I can\'t understand the command. Please say again.')
+        .getResponse();
+    },
+  };
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
