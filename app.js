23758fa18ea9c2382afa79e60615c3d843c84ae2
Fredrick Thornton <fthornton67@gmail.com>
2018-07-15 18:34:50 -0700

    add alexa handler

--- app.js
+++ app.js
@@ -2,6 +2,27 @@
 require('dotenv-extended').load();
 const Alexa = require('ask-sdk');
 
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+      const speechText = 'Welcome to the phitrus skill, say hello!';
+  
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .withSimpleCard('Hello World', speechText)
+        .getResponse();
+    },
+  };
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(LaunchRequestHandler)
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
 
 
 var builder = require('botbuilder');
